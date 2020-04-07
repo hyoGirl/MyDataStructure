cd2507b6a8d20b6577f92bc23ea53f0e492a04e2
xulei <594065953@qq.com>
2020-04-07 17:25:11 +0800

    增加代码

---
+++ src/main/java/com/xulei/leetcode/Easy/Stack/StackTOQueue.java
@@ -0,0 +1,49 @@
+package com.xulei.leetcode.Easy.Stack;
+
+import java.util.Stack;
+
+/**
+ * author:徐磊
+ * Date:2020/4/7
+ * Time:16:39
+ * Description:
+ */
+public class StackTOQueue {
+
+//    实现一个MyQueue类，该类用两个栈来实现一个队列。
+
+    Stack<Integer> stack1=new Stack<>();
+    Stack<Integer> stack2=new Stack<>();
+
+    public MyQueue() {
+
+    }
+
+
+    /**
+     *  栈是先进后出，队列是先进先出
+     */
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+
+
+
+
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+
+    }
+
+    /** Get the front element. */
+    public int peek() {
+
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+
+    }
+}
